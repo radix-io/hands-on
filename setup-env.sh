709e1ea51a38614dc59bab9b3bc8d09e114bf492
Phil Carns <carns@mcs.anl.gov>
2020-07-26 19:39:50 +0000

    update and rename theta setup script

--- setup-env.sh
+++
@@ -1,6 +0,0 @@
-# modules needed for examples on theta.alcf.anl.gov
-module add cray-parallel-netcdf
-module add cray-netcdf
-module add texlive
-
-# additional environment tweaks: none needed on Theta at this time
---
+++ theta-setup-env.sh
@@ -0,0 +1,7 @@
+# modules needed for ATPESC 2020 I/O examples on theta.alcf.anl.gov
+module add cray-parallel-netcdf
+module add cray-netcdf
+module add texlive
+
+# additional environment tweaks:
+export PERLLIB=/projects/ATPESC2020/atpesc-io/perl5
