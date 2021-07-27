95f1444a1e49610e599d96b3d5e9b1ff11656147
Rob Latham <robl@mcs.anl.gov>
2021-07-27 20:34:41 +0000

    updated modules for 2021

--- theta-setup-env.sh
+++ theta-setup-env.sh
@@ -1,7 +1,13 @@
-# modules needed for ATPESC 2020 I/O examples on theta.alcf.anl.gov
+# modules needed for ATPESC 2021 I/O examples on theta.alcf.anl.gov
 module add cray-parallel-netcdf
-module add cray-netcdf
+module add cray-hdf5-parallel
+module add cray-netcdf-hdf5parallel
 module add texlive
+
+# the 'miniconda' distribution contains mpi4py, numpy, and several other useful
+# python modules
+
+module add miniconda-3/latest
 
 # additional environment tweaks:
 export PERLLIB=/projects/ATPESC2020/atpesc-io/perl5
