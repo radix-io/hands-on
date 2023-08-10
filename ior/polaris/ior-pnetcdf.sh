7bb19020de2e02a1732da8f7265056272ffdbf91
Rob Latham <robl@mcs.anl.gov>
2023-08-10 14:35:59 +0000

    minor adjustments to "IOR to pnetcdf" case
    want to really emphasize the effect of two-phase aggregation

--- ior/polaris/ior-pnetcdf.sh
+++ ior/polaris/ior-pnetcdf.sh
@@ -17,13 +17,15 @@ IOR=${HOME}/soft/polaris/ior-4.0.0rc1
 PATH=${IOR}/bin:${PATH}
 OUTPUT=/eagle/ATPESC2023/usr/$USER/ior
 
-mkdir -p $OUTPUT
-lfs setstripe -c -1 $OUTPUT
 
-export MPICH_MPIIO_HINTS_DISPLAY=1
 # experiment: collect DXT traces of collective I/O and pnetcdf
 export DXT_ENABLE_IO_TRACE=1
 
+# I'd like the darshan report to highlight some collective I/O optimizaitons so
+# I am deliberately picking a smaller stripe count.
+mkdir -p $OUTPUT
+lfs setstripe -c 4 $OUTPUT
+
 mpiexec --np $((32*NNODES)) \
-  ior --mpiio.showHints -c -a NCMPI \
+  ior --mpiio.showHints --ncmpi.showHints -c -a NCMPI \
        -b 1000000 -t 1000000 -o ${OUTPUT}/ior-pnetcdf.out
