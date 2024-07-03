2603440e2ae538dd60e88567fe6d292ccd9fc4fd
Rob Latham <robl@mcs.anl.gov>
2024-07-03 10:55:51 -0500

    reformat comments to wrap more nicely

--- hello-io/hello-mpiio-view.c
+++ hello-io/hello-mpiio-view.c
@@ -32,21 +32,23 @@ int main(int argc, char **argv)
      * underlying MPI-IO implementation */
     MPI_Info_create(&info);
 
-    /* we can deal with variable length strings but for the sake of this
-     * exercise it will be simpler if all strings are the same length */
-    snprintf(buf, BUFSIZE, "Hello from rank %3d of %3d\n", rank, nprocs);
+    /* we can deal with variable length strings but for the sake of
+     * this exercise it will be simpler if all strings are the same
+     * length */
+    snprintf(buf, BUFSIZE, "Hello from rank %3d of %3d\n", rank,
+            nprocs);
     len = strlen(buf);
 
-    /* noncontiguous in file requres a "file view".  In this example we will
-     * have each process write their strings such that all the 'H' comes first,
-     * then all the 'e' ... etc
+    /* noncontiguous in file requres a "file view".  In this example
+     * we will have each process write their strings such that all
+     * the 'H' comes first, then all the 'e' ... etc
      * so two processes would write HHeelloo... */
     MPI_Datatype viewtype;
     int *displacements;
     displacements = malloc(len*sizeof(*displacements));
 
-    /* each process will write to its own "view" of the file: for two processes
-     * it would look like:
+    /* each process will write to its own "view" of the file: for two
+     * processes it would look like:
      * - - - - - - - - - - - ...
      * then the write would fill in those locations like this:
      * Rank 0:
@@ -54,8 +56,8 @@ int main(int argc, char **argv)
      * Rank 1:
      *  H e l l o  f r o m ...
      *
-     * and we set up displacements so that each process does not step on any
-     * other process: in the file it looks like
+     * and we set up displacements so that each process does not step
+     * on any other process: in the file it looks like
      * HHeelloo  ffrroomm... */
     for (int i=0; i< len; i++)
         displacements[i] = rank+(i*nprocs);
@@ -67,9 +69,10 @@ int main(int argc, char **argv)
                 MPI_MODE_CREATE|MPI_MODE_WRONLY, info, &fh));
     MPI_CHECK(MPI_File_set_view(fh, 0, MPI_CHAR, viewtype, "native", info));
 
-    /* _all means collective.  Even if we had no data to write, we would
-     * still have to make this call.  In exchange for this coordination,
-     * the underlyng library might be able to greatly optimize the I/O */
+    /* _all means collective.  Even if we had no data to write, we
+     * would still have to make this call.  In exchange for this
+     * coordination, the underlyng library might be able to greatly
+     * optimize the I/O */
     MPI_CHECK(MPI_File_write_at_all(fh, offset, buf, len, MPI_CHAR,
                 &status));
 
