9d2bb47ab611325e57366feb6b4e23a9740fc167
Shane Snyder <ssnyder@mcs.anl.gov>
2024-07-31 15:38:35 +0000

    remove old ascent setup script

--- ascent-setup-env.sh
+++
@@ -1,7 +0,0 @@
-# modules needed for ATPESC 2021 I/O examples on ascent.olcf.ornl.gov
-module add parallel-netcdf/
-module add netcdf
-module add darshan-runtime
-module add darshan-util
-
-# additional environment tweaks:
