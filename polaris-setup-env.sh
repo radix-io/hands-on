59059199df60c9ba6b7de8f637bf778f27bcc78b
Shane Snyder <ssnyder@mcs.anl.gov>
2024-07-31 20:50:54 +0000

    use conda+venv to provide pydarshan on Polaris

--- polaris-setup-env.sh
+++ polaris-setup-env.sh
@@ -4,8 +4,11 @@ module add cray-hdf5-parallel
 module add cray-netcdf-hdf5parallel
 module add darshan
 
-# load a more recent Python version
-module add cray-python
-
-# point to manual PyDarshan install in ATPESC scratch space
-export PYTHONPATH=/eagle/projects/ATPESC2024/usr/soft/track-7-io/pydarshan:$PYTHONPATH
+# use conda + venv to provide PyDarshan
+module use /soft/modulefiles ; module load conda; conda activate base
+CONDA_NAME=$(echo ${CONDA_PREFIX} | tr '\/' '\t' | sed -E 's/mconda3|\/base//g' | awk '{print $NF}')
+VENV_DIR="/eagle/projects/ATPESC2024/usr/$USER/venvs/${CONDA_NAME}"
+mkdir -p "${VENV_DIR}"
+python -m venv "${VENV_DIR}" --system-site-packages
+source "${VENV_DIR}/bin/activate"
+PIP_DISABLE_PIP_VERSION_CHECK=1 pip install -q darshan
