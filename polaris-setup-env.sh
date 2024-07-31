b2725a3f1ee29b4bb5d97a4588b2f11bdb798824
Shane Snyder <ssnyder@mcs.anl.gov>
2024-07-31 18:04:00 +0000

    updated job scripts with 2024 details

--- polaris-setup-env.sh
+++ polaris-setup-env.sh
@@ -2,9 +2,6 @@
 module add cray-parallel-netcdf
 module add cray-hdf5-parallel
 module add cray-netcdf-hdf5parallel
-
-# load darshan module using MODULEPATH for now
-export MODULEPATH=/soft/perftools/darshan/darshan-3.4.3/share/craype-2.x/modulefiles/:$MODULEPATH
 module add darshan
 
 # load a more recent Python version
